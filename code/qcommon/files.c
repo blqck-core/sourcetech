638af43f9b6d1a626dc4000710e1de891ad5f8dd
noire.dev <opennoire@gmail.com>
2026-05-02 05:40:25 +0000

    debug addon thing and try to fix

--- code/qcommon/files.c
+++ code/qcommon/files.c
@@ -197,35 +197,38 @@ fileHandle_t FS_FOpenFileAppend( const char *filename ) {
 	fd->handleSync = qfalse;
 
 	return f;
-}
+
 
 int FS_FOpenFileRead( const char *filename, fileHandle_t *file, qboolean uniqueFILE ) {
-	char netpath[MAX_OSPATH];
-	FILE *temp;
-	int length;
+    char netpath[MAX_OSPATH];
+    FILE *temp = NULL;
+    int length;
+    char localFilename[MAX_OSPATH];   // локальная копия
 
-	if ( !filename ) {
-		Com_Error( ERR_FATAL, "FS_FOpenFileRead: NULL 'filename' parameter passed\n" );
-	}
-
-	// Убираем начальный слэш, если есть
-	if ( filename[0] == '/' || filename[0] == '\\' ) {
-		filename++;
-	}
-
-    for(int i = 0; i <= addon_count->integer; i++) {
-    	// Строим путь: <exe_dir>/<filename>
-    	Q_strncpyz( netpath, Sys_DefaultBasePath(), sizeof( netpath ) );
-    	if(i == addon_count->integer) Q_strcat( netpath, sizeof( netpath ), "/" );
-    	else Q_strcat( netpath, sizeof( netpath ), va("/addons/%s/", addon_name[i]->string));
-    	Q_strcat( netpath, sizeof( netpath ), filename );
-    
-    	// Открываем файл
-    	Com_Printf("searching attempt %i: target= %s | addon= %s | file= %s \n", i, netpath, addon_name[i]->string, filename);
-    	temp = Sys_FOpen( netpath, "rb" );
-    	if(temp) break;
+    if ( !filename ) {
+        Com_Error( ERR_FATAL, "FS_FOpenFileRead: NULL 'filename' parameter passed\n" );
     }
-    
+
+    if ( filename[0] == '/' || filename[0] == '\\' ) {
+        filename++;
+    }
+    Q_strncpyz( localFilename, filename, sizeof( localFilename ) );
+
+    // далее везде в цикле юзаем localFilename ибо filename засирается
+    for ( int i = 0; i <= addon_count->integer; i++ ) {
+        Q_strncpyz( netpath, Sys_DefaultBasePath(), sizeof( netpath ) );
+        if ( i == addon_count->integer ) {
+            Q_strcat( netpath, sizeof( netpath ), "/" );
+        } else {
+            Q_strcat( netpath, sizeof( netpath ), va( "/addons/%s/", addon_name[i]->string ) );
+        }
+        Q_strcat( netpath, sizeof( netpath ), localFilename );   // юзаем копию
+
+        Com_Printf("searching attempt %i: target= %s | addon= %s | file= %s \n", i, netpath, addon_name[i]->string, localFilename);
+        temp = Sys_FOpen( netpath, "rb" );
+        if ( temp ) break;
+    }
+
 	if ( !temp ) {
 		if ( file ) {
 			*file = FS_INVALID_HANDLE;
